d16f30211a3f0ff6bec1f3df6cd3b6cf9fb7c4d9
lionelgwk <lionelgwk@icloud.com>
2023-10-07 14:53:21 +0800

    Added dashboard header for /dashboard path

---
+++ components/DashboardHeader.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+export const DashboardHeader = () => {
+    const [showBalance, setShowBalance] = useState(true);
+    const [portfolioData, setPortfolioData] = useState({
+        currentBalance: 10000,
+        previousBalance: 9500, // take it as balance 24 hours ago?
+    });
+
+    
+    const percentageChange =
+        ((portfolioData.currentBalance - portfolioData.previousBalance) /
+            portfolioData.previousBalance) *
+        100;
+
+    return (
+        <div className="rounded-lg bg-white p-4 shadow-md">
+            <div className="mb-4 flex items-center space-x-2">
+                <h2 className="text-2xl font-semibold">Portfolio Dashboard</h2>
+                <div className="flex cursor-pointer items-center">
+                    {/* Toggle visibility of balance */}
+                    <div
+                        className={`${
+                            showBalance ? "bg-opacity-100" : "bg-opacity-20"
+                        }`}
+                        onClick={() => setShowBalance(!showBalance)}
+                    >
+                        <Image
+                            src="/icons8-eye-30.png"
+                            alt=""
+                            width={20}
+                            height={20}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            {/* Balance */}
+            {showBalance ? (
+                <div>
+                    <p className="mb-2 text-xl font-semibold">
+                        Current Balance
+                    </p>
+                    <div className="flex items-center justify-between">
+                        <p className="text-4xl font-bold text-black">
+                            ${portfolioData.currentBalance.toFixed(2)}
+                        </p>
+                        <button className="rounded-xl bg-blue-600 px-4 py-2 mr-4 text-white hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">
+                            + Create Portfolio
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <p className="mb-2 text-xl font-semibold">
+                        Current Balance
+                    </p>
+                    <p className="text-4xl font-bold text-black">•••••••••</p>
+                </div>
+            )}
+            {/* Percentage Change */}
+            <div className="flex items-center space-x-1 ">
+                <p
+                    className={`mt-2 text-base font-bold ${
+                        percentageChange >= 0
+                            ? "text-green-500"
+                            : "text-red-500"
+                    }`}
+                >
+                    +{percentageChange.toFixed(2)}%
+                </p>
+                <span className="mt-2 rounded bg-gray-200 px-2 py-1 text-xs text-gray-700">
+                    <p>24h</p>
+                </span>
+            </div>
+        </div>
+    );
+};
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,21 @@
+import { DashboardHeader } from "@/components/DashboardHeader";
 import { Layout } from "@/components/Layout";
 import React from "react";
 
 const dashboard = () => {
     return (
-        <Layout>
-            <main
-                className={`text-primary flex min-h-screen flex-col items-center justify-between p-10 text-5xl`}
-            >
-                Dashboard Page
-            </main>
-        </Layout>
+        <>
+            <Layout>
+                <main
+                    // className={`text-primary flex min-h-screen flex-col items-center justify-between p-10 text-5xl`}
+                >
+                    {/* Dashboard Page */}
+                    <DashboardHeader/>
+                </main>
+                
+            </Layout>
+
+        </>
     );
 };
 
