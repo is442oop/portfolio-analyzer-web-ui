0752d9c72b191882da6bbe8f263c1f8c42c80dca
Jerome Wong <jeromewongjh@gmail.com>
2023-10-08 15:06:51 +0800

    feat: portfolio history area chart

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,20 +1,17 @@
 import { DashboardHeader } from "@/components/DashboardHeader";
 import { Layout } from "@/components/Layout";
+import PortfolioHistoryChart from "@/components/PortfolioHistoryChart";
 import React from "react";
 
 const dashboard = () => {
     return (
         <>
             <Layout>
-                <main
-                    // className={`text-primary flex min-h-screen flex-col items-center justify-between p-10 text-5xl`}
-                >
-                    {/* Dashboard Page */}
-                    <DashboardHeader/>
+                <main>
+                    <DashboardHeader />
+                    <PortfolioHistoryChart />
                 </main>
-                
             </Layout>
-
         </>
     );
 };
