73f6b462e941034f35e53ea1c7b5f7c5edb067df
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-10-14 23:21:00 +0800

    feat: email auth flow (#21)
    * feat: card ui component
    
    * feat: icon ui component
    
    * chore: + @supabase/auth-helpers-nextjs
    
    * feat: auth middleware
    
    * feat: auth card
    
    * feat: auth page
    
    * - cmdk
    
    * feat: + logout
    
    * feat: shift tw indicator to top right
    
    * chore: + .env.example
    
    * chore: resize sidebar to w-60
    
    * trigger commit
    
    * refactor: use form element
    
    * nits
    
    ---------
    
    Co-authored-by: Jerome Wong <jeromewongjh@gmail.com>

--- components/TailwindIndicator.tsx
+++ components/TailwindIndicator.tsx
@@ -2,7 +2,7 @@ export function TailwindIndicator() {
     if (process.env.NODE_ENV === "production") return null;
 
     return (
-        <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-primary p-5 text-xs  text-white">
+        <div className="fixed right-1 top-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-primary p-5 text-xs  text-white">
             <div className="block sm:hidden">xs</div>
             <div className="hidden sm:block md:hidden">sm</div>
             <div className="hidden md:block lg:hidden">md</div>
---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    console.log("middleware", req.nextUrl.pathname);
+    const res = NextResponse.next();
+    const supabase = createMiddlewareClient({ req, res });
+    const { data, error } = await supabase.auth.getSession();
+    if (error || !data.session?.user) {
+        const url = req.nextUrl.clone();
+        url.pathname = "/auth";
+        return NextResponse.redirect(url);
+    }
+    return res;
+}
+
+export const config = {
+    matcher: [
+        // Match all request paths except for the ones starting with:
+        // - auth (authentication routes)
+        // - api (API routes)
+        // - _next/static (static files)
+        // - _next/image (image optimization files)
+        // - favicon.ico (favicon file)
+        "/((?!api|_next/static|_next/image|favicon.ico|auth/*).*)",
+    ],
+};
