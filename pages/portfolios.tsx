cbf6aea2ae776f8dafd2ebb52c042c3c15d46ce0
lionelgwk <lionelgwk@icloud.com>
2023-10-23 16:56:11 +0800

    placed under portfolios page, need help with a few refactors

--- pages/portfolios.tsx
+++ pages/portfolios.tsx
@@ -5,14 +5,12 @@ import React from "react";
 const portfolios = () => {
     return (
         <Layout>
-            {/* <main
+            <main
                 className={`flex min-h-screen flex-col items-center justify-between p-10 text-5xl text-primary`}
             >
                 Portfolios Page
-            </main> */}
-
-            <AddTransactionModal/>
-            
+                <AddTransactionModal />
+            </main>
         </Layout>
     );
 };
