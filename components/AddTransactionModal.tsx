d083e29f70fb97b3d4dbfd8d10c3195f2c9062f8
lionelgwk <lionelgwk@icloud.com>
2023-10-24 22:44:21 +0800

    added disable to button if any input field was not filled up

--- components/AddTransactionModal.tsx
+++ components/AddTransactionModal.tsx
@@ -244,6 +244,7 @@ export const AddTransactionModal = () => {
                                 onChange={(e) =>
                                     setQuantity(Number(e.target.value))
                                 }
+                                required
                             />
                         </div>
                         <div className="max-w-fit items-center gap-1.5">
@@ -301,11 +302,10 @@ export const AddTransactionModal = () => {
 
                     <DialogClose
                         type="submit"
-                        // disabled={portfolioName.length == 0}
+                        disabled={quantity == 0 || stockPrice == 0 || !date || !value}
                         className="mt-4 inline-flex h-10 w-full items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
                     >
                         <p className="font-bold">Add Transaction</p>
-                        {/* </Button> */}
                     </DialogClose>
                 </form>
             </DialogContent>
