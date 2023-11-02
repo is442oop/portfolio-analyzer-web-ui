1405d0b578bf9f6e2131aa6e0a8eeeb94bd060d6
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-11-03 03:41:20 +0800

    feat: ui and routing for portfolio list

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,7 +7,7 @@ import AssetTable from "@/components/AssetTable";
 const dashboard = () => {
     return (
         <Layout>
-            <div className="p-10">
+            <div className="h-screen py-10 pl-0 pr-10 sm:p-10">
                 <DashboardHeader />
                 <div className="flex flex-col gap-3 lg:flex-row">
                     <PortfolioHistoryChart />
