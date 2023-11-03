794b122670b7e976b52cbdaf6891f23edc4ceb35
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-11-03 16:27:44 +0800

    feat: create portfolio endpoint

---
+++ pages/api/portfolios/index.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    if (req.method === "POST") {
+        try {
+            const response = await axios.post(
+                `${process.env.API_URL}/api/portfolio`,
+                req.body,
+            );
+            res.status(200).json(response.data);
+        } catch (error) {
+            return res.status(500).json({ message: error });
+        }
+    }
+}
