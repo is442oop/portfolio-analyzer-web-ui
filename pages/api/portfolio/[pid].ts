ddddb02186beedda0a2ee27583193e134b54de8e
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-11-05 01:59:11 +0800

    refactor: api endpoints structure

--- pages/api/portfolio/[pid].ts
+++ pages/api/portfolio/[pid].ts
@@ -6,13 +6,28 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
-    try {
-        const { pid } = req.query;
-        const response = await axios.get(
-            `${process.env.API_URL}/api/portfolio/${pid}`,
-        );
-        return res.status(200).json(response.data);
-    } catch (error) {
-        return res.status(500).json({ message: error });
+    if (req.method === "GET") {
+        try {
+            const { pid } = req.query;
+            const response = await axios.get(
+                `${process.env.API_URL}/api/portfolio/${pid}`,
+            );
+            return res.status(200).json(response.data);
+        } catch (error) {
+            return res.status(500).json({ message: error });
+        }
+    }
+
+    if (req.method === "PUT") {
+        try {
+            const { pid } = req.query;
+            const response = await axios.put(
+                `${process.env.API_URL}/api/portfolio/${pid}`,
+                req.body,
+            );
+            return res.status(200).json(response.data);
+        } catch (error) {
+            return res.status(500).json({ message: error });
+        }
     }
 }
