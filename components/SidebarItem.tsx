e3348ff762842114db44d2378e74ffd89c0f3b49
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-11-04 17:35:12 +0800

    fix: persist sidebar active state on subroutes

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -10,18 +10,20 @@ interface SidebarItemProps {
 }
 
 const SidebarItem = ({ link, location, open }: SidebarItemProps) => {
+    const parentPath = location.pathname.split("/")[1];
+
     return (
         <Link
             href={link.href}
             className={cn("border-l-4 border-transparent", {
-                " border-l-4 border-primary": location.pathname === link.href,
+                " border-l-4 border-primary": "/" + parentPath === link.href,
             })}
         >
             <li
                 className={cn(
                     "flex cursor-pointer items-center gap-x-2 rounded-lg px-2 py-1 font-normal text-foreground hover:bg-border",
                     {
-                        "rounded-l-none": location.pathname === link.href,
+                        "rounded-l-none": "/" + parentPath === link.href,
                     },
                 )}
             >
