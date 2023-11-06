637cfc93c4d28d3928f697d8272b66dd90e8fd4a
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-11-06 15:14:05 +0800

    feat: get industry allocation endpoint

---
+++ pages/api/portfolio/[pid]/allocation/industry.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+// This endpoint gets allocation of assets based on industry for a given portfolio id
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    if (req.method === "GET") {
+        try {
+            const { pid } = req.query;
+            const response = await axios.get(
+                `${process.env.API_URL}/api/portfolio/${pid}/allocation/industry`,
+            );
+
+            console.log("pre", response.data);
+            // format the percentage to * 100 and round to 2 decimal places
+            response.data.forEach((element: any) => {
+                element.percentage = parseFloat(
+                    (element.percentage * 100).toFixed(2),
+                );
+            });
+
+            console.log("post", response.data);
+            return res.status(200).json(response.data);
+        } catch (error) {
+            return res.status(500).json({ message: error });
+        }
+    }
+}
