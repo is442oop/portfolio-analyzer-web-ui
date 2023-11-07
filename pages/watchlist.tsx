8d9dad73faaec3e279366d97a9fab24ea31c9a13
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-11-08 05:03:11 +0800

    + perspective component

--- pages/watchlist.tsx
+++ pages/watchlist.tsx
@@ -2,11 +2,12 @@ import { Layout } from "@/components/Layout";
 import UserWatchlist from "@/components/UserWatchlist";
 import { Button } from "@/components/ui/Button";
 import React from "react";
+import PerspectiveViewer from "@/components/PerspectiveViewer";
 
 const watchlist = () => {
     return (
         <Layout>
-            <div className="h-fit min-h-screen py-10 pl-0 pr-10 sm:p-10">
+            <div className="flex h-full flex-col items-stretch py-10 pl-0 pr-10 sm:p-10">
                 <div className="flex items-center justify-between rounded-lg bg-white py-4">
                     <h2 className="text-2xl font-semibold text-primary">
                         Watchlist
@@ -15,6 +16,7 @@ const watchlist = () => {
                     <Button> + Ticker</Button>
                 </div>
                 <UserWatchlist />
+                <PerspectiveViewer />
             </div>
         </Layout>
     );
