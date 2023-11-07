711e845a4831ba6592cc2b1b67a0310e42229606
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-11-07 12:20:42 +0800

    feat: asset allocation api and ui

--- components/AllocationPieChart.tsx
+++ components/AllocationPieChart.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
     PieChart,
     Pie,
@@ -12,36 +11,43 @@ const COLORS = ["#6477AF", "#749AC7", "#85B5D0", "#97CDD8"];
 
 const AllocationPieChart = ({
     allocationData,
+    selectedAllocation,
 }: {
     allocationData: AllocationChartProps[];
+    selectedAllocation: string;
 }) => {
+    console.log(allocationData);
     return (
-        <ResponsiveContainer width="100%" height={400}>
-            <PieChart className="">
-                <Pie
-                    nameKey="stock"
-                    dataKey="percentage"
-                    data={allocationData}
-                    cx="50%"
-                    cy="50%"
-                    innerRadius={60}
-                    outerRadius={90}
-                    fill="#8884d8"
-                    paddingAngle={0.5}
-                    label
-                    isAnimationActive={false}
-                >
-                    {allocationData.map((entry, index) => (
-                        <Cell
-                            key={`cell-${index}`}
-                            fill={COLORS[index % COLORS.length]}
-                        />
-                    ))}
-                </Pie>
-                <Tooltip />
-                <Legend />
-            </PieChart>
-        </ResponsiveContainer>
+        <>
+            {selectedAllocation && allocationData && (
+                <ResponsiveContainer width="100%" height={400}>
+                    <PieChart>
+                        <Pie
+                            animationDuration={800}
+                            nameKey={selectedAllocation}
+                            dataKey="percentage"
+                            data={allocationData}
+                            cx="50%"
+                            cy="50%"
+                            innerRadius={60}
+                            outerRadius={90}
+                            fill="#8884d8"
+                            paddingAngle={0.5}
+                            label
+                        >
+                            {allocationData.map((entry, index) => (
+                                <Cell
+                                    key={`cell-${index}`}
+                                    fill={COLORS[index % COLORS.length]}
+                                />
+                            ))}
+                        </Pie>
+                        <Legend />
+                        <Tooltip />
+                    </PieChart>
+                </ResponsiveContainer>
+            )}
+        </>
     );
 };
 
--- components/AssetAllocationChart.tsx
+++ components/AssetAllocationChart.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import { ChartControls } from "./ChartControls";
 import AllocationPieChart from "./AllocationPieChart";
 import { useQuery } from "react-query";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const AssetAllocationChart = ({
     isIndividualPortfolio,
@@ -12,16 +12,16 @@ const AssetAllocationChart = ({
     pid?: number;
 }) => {
     const router = useRouter();
-    // TODO: when selectedPreiod changes, fetch new data and memoize it
-    const [selectedPeriod, setSelectedPeriod] = useState("Industry");
+    const [selectedAllocation, setSelectedAllocation] = useState("Industry");
 
     const getIndivAllocationData = async () => {
         const response = await fetch(
-            `/api/portfolio/${router.query.pid}/allocation/industry`,
+            `/api/portfolio/${
+                router.query.pid
+            }/allocation/${selectedAllocation.toLowerCase()}`,
         );
         return await response.json();
     };
-
     const getAllPortfolioAllocationData = async () => {
         return [
             { stock: "AAPL", percentage: 40 },
@@ -31,26 +31,41 @@ const AssetAllocationChart = ({
         ];
     };
 
-    const { data: assetAllocationData, isLoading: allocationDataLoading } =
-        useQuery<AllocationChartProps[]>(
-            "allocationData",
-            isIndividualPortfolio
-                ? getIndivAllocationData
-                : getAllPortfolioAllocationData,
-            { enabled: router.isReady },
-        );
+    const {
+        data: assetAllocationData,
+        isLoading: allocationDataLoading,
+        refetch,
+    } = useQuery<AllocationChartProps[]>(
+        "allocationData",
+        isIndividualPortfolio
+            ? getIndivAllocationData
+            : getAllPortfolioAllocationData,
+        { enabled: router.isReady },
+    );
+
+    useEffect(() => {
+        refetch();
+    }, [selectedAllocation]);
+
     return (
         <div className="mx-auto w-full space-y-2 rounded-xl border px-1 py-5 xl:w-1/2">
             <div className="flex flex-col items-center justify-center gap-y-2 font-bold">
                 <h1>Asset Allocation</h1>
                 <ChartControls
-                    selectedPeriod={selectedPeriod}
-                    setSelectedPeriod={setSelectedPeriod}
-                    periods={["Industry", "Stocks"]}
+                    selectedPeriod={selectedAllocation}
+                    setSelectedPeriod={setSelectedAllocation}
+                    periods={["Industry", "Ticker"]}
                 />
             </div>
             {assetAllocationData && (
-                <AllocationPieChart allocationData={assetAllocationData} />
+                <AllocationPieChart
+                    selectedAllocation={
+                        selectedAllocation === "Industry"
+                            ? "industry"
+                            : "assetTicker"
+                    }
+                    allocationData={assetAllocationData}
+                />
             )}
         </div>
     );
