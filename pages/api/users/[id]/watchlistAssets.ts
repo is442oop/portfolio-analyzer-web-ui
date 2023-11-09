790df8ae1f7924282cb0f8f76ddab0ba5be51590
lionelgwk <lionelgwk@icloud.com>
2023-11-09 15:14:09 +0800

    changed index values

--- pages/api/users/[id]/watchlistAssets.ts
+++ pages/api/users/[id]/watchlistAssets.ts
@@ -33,11 +33,11 @@ export default async function handler(
             ) as number[];
             const timeSeries7days = timeSeries60minArray.slice(0, 168);
             const percent24h =
-                (timeSeries60minArray[0] - timeSeries60minArray[23]) /
-                timeSeries60minArray[23];
+                (timeSeries60minArray[0] - timeSeries60minArray[15]) /
+                timeSeries60minArray[15];
             const percent7d =
-                (timeSeries60minArray[0] - timeSeries60minArray[167]) /
-                timeSeries60minArray[167];
+                (timeSeries60minArray[0] - timeSeries60minArray[111]) /
+                timeSeries60minArray[111];
             const price = tickerdata["Global Quote Price"];
             const volume24h = Number(tickerdata["Global Quote Volume"]);
             const marketCap = Number(
