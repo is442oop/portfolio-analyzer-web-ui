9266e4dd7a1f600da74ed070f12483e71885fc53
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-11-09 13:48:13 +0800

    chore: optimization to watchlist

--- pages/api/users/[id]/watchlistAssets.ts
+++ pages/api/users/[id]/watchlistAssets.ts
@@ -6,56 +6,66 @@ export default async function handler(
     res: NextApiResponse<WatchlistAsset[]>,
 ) {
     const { id } = req.query;
-    const assets = [];
-    const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
-
-    let watchlistTickers = [];
     let toRet: TickerPriceData[] = [];
 
     try {
-        const response = await axios.get(`${process.env.API_URL}/api/watchlist/user/${id}/prices`);
-        const data = await response.data["watchlistAssetPriceMap"];
-        const responseTicker = await axios.get(`${process.env.API_URL}/api/watchlist/user/${id}`);
-        const dataTicker = await responseTicker.data;
-        watchlistTickers = dataTicker.watchlist_assets;
+        const watchlistAssetPriceMapPromise = axios
+            .get(`${process.env.API_URL}/api/watchlist/user/${id}/prices`)
+            .then(async (r) => {
+                return await r.data.watchlistAssetPriceMap;
+            });
+        const watchlistTickersPromise = axios
+            .get(`${process.env.API_URL}/api/watchlist/user/${id}`)
+            .then(async (r) => {
+                return await r.data.watchlist_assets;
+            });
 
-            for (let i = 0; i < watchlistTickers.length; i++) {
-                let ticker = watchlistTickers[i];
-                let tickerdata = data[i][ticker];
-                const timeSeries60minArray = Object.values(
-                    tickerdata["AssetIntraday"]
-                ) as number[];
+        const [watchlistAssetPriceMap, watchlistTickers] = await Promise.all([
+            watchlistAssetPriceMapPromise,
+            watchlistTickersPromise,
+        ]);
 
-            
-                const timeSeries7days = timeSeries60minArray.slice(0, 113);
-                const percent24h = (timeSeries60minArray[0] - timeSeries60minArray[15]) / timeSeries60minArray[15];
-                const percent7d = (timeSeries60minArray[0] - timeSeries60minArray[111]) / timeSeries60minArray[111];
-                const price = tickerdata["Global Quote Price"];
-                const volume24h = Number(tickerdata["Global Quote Volume"]);
-                const marketCap = Number(tickerdata["AssetOverview MarketCapitalization"]);
-                const name = tickerdata["AssetOverview Name"];
-                const chartData = timeSeries7days
-                    .map((item) => ({
-                        value: item,
-                    }))
-                    .reverse();
+        for (let i = 0; i < watchlistTickers.length; i++) {
+            let ticker = watchlistTickers[i];
+            let tickerdata = watchlistAssetPriceMap[i][ticker];
+            const timeSeries60minArray = Object.values(
+                tickerdata["AssetIntraday"],
+            ) as number[];
+            const timeSeries7days = timeSeries60minArray.slice(0, 168);
+            const percent24h =
+                (timeSeries60minArray[0] - timeSeries60minArray[23]) /
+                timeSeries60minArray[23];
+            const percent7d =
+                (timeSeries60minArray[0] - timeSeries60minArray[167]) /
+                timeSeries60minArray[167];
+            const price = tickerdata["Global Quote Price"];
+            const volume24h = Number(tickerdata["Global Quote Volume"]);
+            const marketCap = Number(
+                tickerdata["AssetOverview MarketCapitalization"],
+            );
+            const name = tickerdata["AssetOverview Name"];
+            const chartData = timeSeries7days
+                .map((item) => ({
+                    value: item,
+                }))
+                .reverse();
 
-                toRet.push({
-                    ticker: ticker,
-                    name: name,
-                    price: price,
-                    price24hDeltaPercentage: percent24h,
-                    price7dDeltaPercentage: percent7d,
-                    marketCap: marketCap,
-                    volume24h: volume24h,
-                    sparkline: chartData,
-                });
-            }
+            toRet.push({
+                ticker: ticker,
+                name: name,
+                price: price,
+                price24hDeltaPercentage: percent24h,
+                price7dDeltaPercentage: percent7d,
+                marketCap: marketCap,
+                volume24h: volume24h,
+                sparkline: chartData,
+            });
+        }
         // }
         res.status(200).json(
             toRet.map((toRet) => {
                 return { ...toRet };
-            })
+            }),
         );
     } catch (error) {
         console.log(error);
